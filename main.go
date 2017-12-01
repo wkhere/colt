e89b45150a363e689fdff2a78cb754aa6fa0a6f9
Wojciech Kaczmarek <wojtekk@kofeina.net>
2017-12-01 20:29:05 +0100

    benchmarks + configurable stderr

--- main.go
+++ main.go
@@ -17,14 +17,14 @@ func dieIf(err error) {
 }
 
 type columnProc struct {
-	separator rune
-	selection int
-	command   []string
-	output    io.Writer
+	separator      rune
+	selection      int
+	command        []string
+	stdout, stderr io.Writer
 }
 
 func main() {
-	p := columnProc{output: os.Stdout}
+	p := columnProc{stdout: os.Stdout, stderr: os.Stderr}
 
 	p.parseArgs(os.Args[1:])
 
@@ -32,7 +32,7 @@ func main() {
 
 	for scanner.Scan() {
 		p.process(scanner.Text())
-		fmt.Fprintln(p.output)
+		fmt.Fprintln(p.stdout)
 	}
 
 	dieIf(scanner.Err())
@@ -57,8 +57,8 @@ func (p *columnProc) process(line string) {
 
 	selectedIdx, err := setupIdx(p.selection, len(cols))
 	if err != nil {
-		warn(err)
-		fmt.Fprint(p.output, line)
+		p.warn(err)
+		fmt.Fprint(p.stdout, line)
 		return
 	}
 
@@ -68,7 +68,7 @@ func (p *columnProc) process(line string) {
 				p.processData(token.val)
 				continue
 			}
-			fmt.Fprint(p.output, token.val)
+			fmt.Fprint(p.stdout, token.val)
 		}
 	}
 }
@@ -77,12 +77,12 @@ func (p *columnProc) processData(s string) {
 	var b bytes.Buffer
 	cmd := exec.Command(p.command[0], append(p.command[1:], s)...)
 	cmd.Stdout = &b
-	cmd.Stderr = os.Stderr
+	cmd.Stderr = p.stderr
 	err := cmd.Run()
 	dieIf(err)
-	p.output.Write(bytes.TrimRight(b.Bytes(), "\r\n"))
+	p.stdout.Write(bytes.TrimRight(b.Bytes(), "\r\n"))
 }
 
-func warn(err error) {
-	fmt.Fprintf(os.Stderr, "WARN %v\n", err)
+func (p *columnProc) warn(err error) {
+	fmt.Fprintf(p.stderr, "WARN %v\n", err)
 }
--- main_test.go
+++ main_test.go
@@ -2,47 +2,64 @@ package main
 
 import (
 	"bytes"
+	"io/ioutil"
 	"testing"
 )
 
+var cmd = []string{"perl", "-e", `print uc $ARGV[0], "\r\n"`}
+
+var tab = []struct {
+	sel    int
+	input  string
+	output string
+}{
+	{1, "aa", "AA"},
+	{1, "aa;bb", "AA;bb"},
+	{1, "aa; bb", "AA; bb"},
+	{1, "aa ;bb", "AA ;bb"},
+	{1, "aa ; bb", "AA ; bb"},
+	{1, " aa; bb", " AA; bb"},
+	{2, "aa;bb", "aa;BB"},
+	{2, "aa;bb ", "aa;BB "},
+	{2, "aa; bb", "aa; BB"},
+	{2, "aa ;bb", "aa ;BB"},
+	{2, "aa ; bb", "aa ; BB"},
+	{3, "aa;bb", "aa;bb"},
+	{-1, "aa", "AA"},
+	{-1, "aa;bb", "aa;BB"},
+	{-2, "aa;bb", "AA;bb"},
+	{-3, "aa;bb", "aa;bb"},
+	{0, "aa;bb", "aa;bb"},
+}
+
 func TestProcess(t *testing.T) {
-	var cmd = []string{"perl", "-e", `print uc $ARGV[0], "\r\n"`}
-
-	var tab = []struct {
-		sel    int
-		input  string
-		output string
-	}{
-		{1, "aa", "AA"},
-		{1, "aa;bb", "AA;bb"},
-		{1, "aa; bb", "AA; bb"},
-		{1, "aa ;bb", "AA ;bb"},
-		{1, "aa ; bb", "AA ; bb"},
-		{1, " aa; bb", " AA; bb"},
-		{2, "aa;bb", "aa;BB"},
-		{2, "aa;bb ", "aa;BB "},
-		{2, "aa; bb", "aa; BB"},
-		{2, "aa ;bb", "aa ;BB"},
-		{2, "aa ; bb", "aa ; BB"},
-		{3, "aa;bb", "aa;bb"},
-		{-1, "aa", "AA"},
-		{-1, "aa;bb", "aa;BB"},
-		{-2, "aa;bb", "AA;bb"},
-		{-3, "aa;bb", "aa;bb"},
-		{0, "aa;bb", "aa;bb"},
-	}
-
 	for i, tc := range tab {
 		var b bytes.Buffer
 		p := columnProc{
 			separator: ';',
 			selection: tc.sel,
 			command:   cmd,
-			output:    &b,
+			stdout:    &b,
+			stderr:    ioutil.Discard,
 		}
 		p.process(tc.input)
 		if res := b.String(); res != tc.output {
 			t.Errorf("tc[%d] mismatch\ngot %v\nexp %v", i, res, tc.output)
+		}
+	}
+}
+
+func BenchmarkProcess(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tab {
+			p := columnProc{
+				separator: ';',
+				selection: tc.sel,
+				command:   cmd,
+				stdout:    ioutil.Discard,
+				stderr:    ioutil.Discard,
+			}
+			p.process(tc.input)
 		}
 	}
 }
