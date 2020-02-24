07932b95814365630ce8040c01a9047de21adc38
Wojciech Kaczmarek <wojtekk@kofeina.net>
2020-02-24 03:22:00 +0100

    cmd: supercool main test via pipe/TeeReader

---
+++ cmd/colt/main_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+func ExampleMain() {
+
+	os.Args = os.Args[:1]
+	os.Args = append(os.Args,
+		`perl`, `-e`, `print "[", uc($ARGV[0]), "]"`)
+
+	b := new(bytes.Buffer)
+	b.WriteString("a;b;c\n")
+	b.WriteString("a;b;c d\n")
+	b.WriteString(`a;b;"c;d"` + "\n")
+
+	feed(&os.Stdin, b)
+	main()
+
+	// Output:
+	// a;b;[C]
+	// a;b;[C D]
+	// a;b;["C;D"]
+}
+
+func feed(fp **os.File, b *bytes.Buffer) {
+	pr, pw, err := os.Pipe()
+	if err != nil {
+		panic(err)
+	}
+	*fp = pr
+	ioutil.ReadAll(io.TeeReader(b, pw))
+	pw.Close()
+}
