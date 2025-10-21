086443184fe4195c08be195f94cb0b77f6f2a625
Wojciech Kaczmarek <wojtekk@kofeina.net>
2025-10-21 16:53:23 +0200

    notes

--- unquote.go
+++ unquote.go
@@ -4,8 +4,8 @@ import "unicode/utf8"
 
 // use strconv.Unquote instead?
 // - might be fine, but also that func unquotes any pair of '"`,
-// then, we should actually fix it (stop thinking about making it
-// configurable, remove from Colt struct and possibly from lex.Config)
+// then, I should stop passing it here, stop making it configurable,
+// remove from args, from the Colt struct and also from lex.Config
 
 func unquote(b []byte, q rune) []byte {
 	l := utf8.RuneLen(q)
